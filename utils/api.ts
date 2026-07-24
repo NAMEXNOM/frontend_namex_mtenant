1eb922c78f7a1c7fac8419148d36f323039335b5
Ramon <ramon.anchondo@gmail.com>
2026-07-24 16:12:39 -0600

    trabajando con tenant frontend 01

--- utils/api.ts
+++ utils/api.ts
@@ -1,4 +1,5 @@
 // utils/api.ts
+/*
 declare const process: any;
 
 export const API_URL = process.env.NEXT_PUBLIC_API_URL || '/api';
@@ -6,3 +7,19 @@ export const API_URL = process.env.NEXT_PUBLIC_API_URL || '/api';
 export const fetchCustom = async (endpoint: string, options: RequestInit = {}) => {
     return fetch(`${API_URL}${endpoint}`, options);
 };
+*/
+
+// utils/api.ts
+
+const getApiUrl = (): string => {
+  if (typeof window !== 'undefined') {
+    const hostname = window.location.hostname; // Extrae "empresademo.namexportal.com"
+    
+    // 🚨 FIX DE EMERGENCIA: Forzamos HTTP plano para evadir el bloqueo de certificados en la demo
+    return `http://${hostname}/api`;
+  }
+  
+  return 'http://127.0.0';
+};
+
+export const API_URL = getApiUrl();
